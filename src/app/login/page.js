353d7b6d45db123f6ac5fd589fb8b01fa8a1032e
Renaldo Louis <renaldo.louis@commsult.id>
2025-05-02 11:20:43 +0700

    add page for login but not integrated to nextAuth

---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import LoginForm from "../components/LoginForm";
+
+export default function Login() {
+    return (
+        <main className="flex h-screen">
+            <div className="bg-white flex justify-center items-center">
+                <LoginForm />
+            </div>
+        </main>
+    )
+}
